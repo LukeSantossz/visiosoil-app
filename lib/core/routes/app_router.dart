7a5a7f2973bfda165539f454ff61b8bc020805f1
LukeSantossz <lucassg2015@gmail.com>
2026-03-28 19:39:08 -0300

    test: sync widget tests with VisioSoil global app properties

--- lib/core/routes/app_router.dart
+++ lib/core/routes/app_router.dart
@@ -2,7 +2,6 @@ import 'package:go_router/go_router.dart';
 import '../features/capture/capture.dart';
 import '../features/history/history.dart';
 import '../features/details/details.dart';
-import '../features/home/home_page.dart';
 import '../features/main/main_screen.dart';
 
 final appRouter = GoRouter(
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:visiosoil_app/core/routes/app_router.dart';
-import 'package:visiosoil_app/core/features/home/home_page.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
