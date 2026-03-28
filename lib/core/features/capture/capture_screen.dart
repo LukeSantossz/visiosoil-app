dcb781f59716e6aa63fc9d098982f93e704ad974
LukeSantossz <lucassg2015@gmail.com>
2026-03-27 22:41:38 -0300

    feat(capture): implement image picker module and ui trigger

--- lib/core/features/capture/capture_screen.dart
+++ lib/core/features/capture/capture_screen.dart
@@ -1,20 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class CaptureScreen extends StatelessWidget {
   const CaptureScreen({super.key});
+
+  Future<void> _pickImage() async {
+    final ImagePicker picker = ImagePicker();
+    final XFile? image = await picker.pickImage(source: ImageSource.camera);
+
+    if (image != null) {
+      debugPrint('Imagem selecionada: ${image.path}');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Captura de Imagens')),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Icon(Icons.camera, size: 50.0),
-            const SizedBox(height: 16.0),
-            const Text('Capture aqui!'),
-          ],
+        child: ElevatedButton(
+          onPressed: _pickImage,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              const Icon(Icons.camera, size: 50.0),
+              const SizedBox(height: 16.0),
+              const Text('Capture aqui!'),
+            ],
+          ),
         ),
       ),
     );
